43250540e2d81ba45b70a4d5ddce172249f845d2
Pavel <zenko_pavel@mail.ru>
2024-10-31 11:08:16 +0300

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import CostItem from './components/CostItem';
+import Costs from "./components/Costs/Costs";
 
-export default function App() {
-  const data = [
+const App = () => {
+  const costs = [
     {
-      date: '12.04.2024',
-      description: 'Холодильник',
-      price: '500'
+      date: new Date(2021, 2, 12),
+      description: "Холодильник",
+      amount: 999.99,
     },
     {
-      date: '12.04.2024',
-      description: 'Телефон',
-      price: '1000'
-    }
-  ]
+      date: new Date(2021, 11, 25),
+      description: "MacBook",
+      amount: 1254.72,
+    },
+    {
+      date: new Date(2021, 3, 1),
+      description: "Джинсы",
+      amount: 49.99,
+    },
+  ];
 
   return (
     <div>
-      <CostItem date={data[0].date} description={data[0].description} price={data[0].price} />
-      <CostItem date={data[1].date} description={data[1].description} price={data[1].price} />
+      <h1>Начнём изучение React!</h1>
+      <Costs costs={costs} />
     </div>
   );
-}
+};
+
+export default App;
